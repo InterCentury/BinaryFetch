fb6b496bd9475ad03c4491fb9f35272c650697b7
BinaryOxide <234583143+BinaryOxide@users.noreply.github.com>
2025-12-23 23:47:27 +0600

    test: tired to implement ansi colors in header result: working fine

--- project_binary_fetch/binary_fetch_v1/main.cpp
+++ project_binary_fetch/binary_fetch_v1/main.cpp
@@ -76,6 +76,24 @@ int main() {
         // Program continues even if art fails to load
     }
 
+
+    // ANSI color definitions
+        const std::string reset = "\033[0m";
+    const std::string red = "\033[31m";
+    const std::string green = "\033[32m";
+    const std::string yellow = "\033[33m";
+    const std::string blue = "\033[34m";
+    const std::string magenta = "\033[35m";
+    const std::string cyan = "\033[36m";
+    const std::string white = "\033[37m";
+    const std::string bright_red = "\033[91m";
+    const std::string bright_green = "\033[92m";
+    const std::string bright_yellow = "\033[93m";
+    const std::string bright_blue = "\033[94m";
+    const std::string bright_magenta = "\033[95m";
+    const std::string bright_cyan = "\033[96m";
+    const std::string bright_white = "\033[97m";
+
     // Create LivePrinter
     LivePrinter lp(art);
 
@@ -125,8 +143,11 @@ int main() {
 
     //BinaryFetch Header-----------------------------------------------#
     {
+        // Colored header
         std::ostringstream ss;
-        ss << "~>> " << "BinaryFetch" << "_____________________________________________________";
+        ss << red << "~>> " << reset
+            << green << "BinaryFetch" << reset
+            << red << "_____________________________________________________" << reset;
         lp.push(ss.str());
     }
     //---------------------beginning of compact section-----------------------
