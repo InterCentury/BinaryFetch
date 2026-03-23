3041e6657c31206888233b56cf48e551538f38fa
InterCentury <234583143+InterCentury@users.noreply.github.com>
2026-03-24 03:41:47 +0600

    Refactor: remove std:: with "using namespace std:" file: CompactCPU.h

--- .history/project_binary_fetch/binary_fetch_v1/include/CompactCPU_20260323071136.h
+++ .history/project_binary_fetch/binary_fetch_v1/include/CompactCPU_20260323071136.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <string>
-using namespace std;
+
 class CompactCPU {
 public:
-    string getCPUName();
-    string getCPUCores();      // returns "6" (core)
-    string getCPUThreads();    // returns "12" (threds)
+    std::string getCPUName();
+    std::string getCPUCores();      // returns "6" (core)
+    std::string getCPUThreads();    // returns "12" (threds)
     double getClockSpeed();         // returns 4.40 (ghz)
     double getUsagePercent();       // returns 12.5 (usage in double)
 };
