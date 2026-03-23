22e763a685d0076b1cd38181c043a67bcdf90eab
InterCentury <234583143+InterCentury@users.noreply.github.com>
2026-03-24 03:37:13 +0600

    refactor: remove std:: resence from compact_disk_info.h

--- project_binary_fetch/binary_fetch_v1/include/compact_disk_info.h
+++ project_binary_fetch/binary_fetch_v1/include/compact_disk_info.h
@@ -1,21 +1,22 @@
 #pragma once
 #include <vector>
 #include <string>
+using namespace std;
 
 class DiskInfo {
 public:
     DiskInfo();  // Constructor
 
     // Get usage of all disks as integers (%)
-    std::vector<std::pair<std::string, int>> getAllDiskUsage();
+    vector<pair<string, int>> getAllDiskUsage();
 
     // Get capacity of all disks as integers (GB)
-    std::vector<std::pair<std::string, int>> getDiskCapacity();
+    vector<pair<string, int>> getDiskCapacity();
 
 private:
     // Helper: calculate used percentage
-    int calculateUsedPercentage(const std::string& driveLetter);
+    int calculateUsedPercentage(const string& driveLetter);
 
     // Helper: calculate total capacity in GB
-    int calculateCapacityGB(const std::string& driveLetter);
+    int calculateCapacityGB(const string& driveLetter);
 };
